3d8fcd5fffb02c4bef515a919f973dd4592e9fd4
Saadatul <Shashaadatul26@gmail.com>
2017-02-08 08:19:57 +0700

    Finish Commit

---
+++ src/programaritmatikaa/ProgramAritmatikaa.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programaritmatikaa;
+
+/**
+ *
+ * @author -asus-
+ */
+public class ProgramAritmatikaa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
